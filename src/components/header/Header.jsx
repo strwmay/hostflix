853e58f378675a3ce2342adfb3d33e085fdee7ea
May <senaidev.may@gmail.com>
2025-03-26 15:02:03 -0300

    removendo imagens png e adicionando svg e icon + ajustes no bootstrap header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
-import Logo from "../../assets/logo-hostflix.png";
-import Lupa from "../../assets/search.png";
+import Logo from "../../assets/logo-hostflix.svg";
+import { Search } from "react-bootstrap-icons";
 import "./Header.css";
 
 const Header = ({ search, setSearch, searchMovies }) => {
@@ -8,16 +8,27 @@ const Header = ({ search, setSearch, searchMovies }) => {
   };
 
   return (
-    <header className="header">
-      <img className="logo" src={Logo} alt="logo" />
-      <div className="search">
+    <header className="header d-flex flex-column flex-md-row justify-content-between align-items-center p-2">
+      <img
+        className="logo img-fluid"
+        src={Logo}
+        alt="logo"
+        style={{ maxWidth: "250px" }}
+      />
+      <div className="search rounded-1 mt-3 input-group w-75 py-1 px-3 align-items-center">
         <input
           onKeyDown={handleKeyPress}
           onChange={(e) => setSearch(e.target.value)}
           type="text"
+          className="form-control bg-transparent form-control-sm border-0 fs-5 fw-regular"
           placeholder="Busque aqui..."
         />
-        <img onClick={() => searchMovies(search)} src={Lupa} alt="Pesquisar" />
+        <Search
+          onClick={() => searchMovies(search)}
+          role="button"
+          className="search-icon "
+          size={30}
+        />
       </div>
     </header>
   );
